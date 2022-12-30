1f3fc3e02313d7b494a04e24885e122c6e027a36
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-30 15:41:46 +0300

    Auto migration added.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,12 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Business.IoC.Autofac;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CustomerInformationDbContext>();
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureContainer<ContainerBuilder>(builder =>
@@ -15,6 +19,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<CustomerInformationDbContext>();
+    dataContext.Database.EnsureCreated();
+}
+
 
 if (app.Environment.IsDevelopment())
 {
